fc2497d8a1644fb5425f827aaa572bc151288635
David Aristakesyan <45460144+DavoA@users.noreply.github.com>
2024-05-17 20:37:20 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
 module "my-lambda" {
 	source = "./modules"
+	function_name                  = "Lambda_Function"
+        handler                        = "index.lambda_handler"
+        runtime                        = "python3.8"
 }
