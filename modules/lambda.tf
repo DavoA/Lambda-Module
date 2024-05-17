5b1d09ae12aaad0e95a7aff160b075ee3b7b9d14
David Aristakesyan <45460144+DavoA@users.noreply.github.com>
2024-05-17 20:36:11 +0400

    Update lambda.tf

--- modules/lambda.tf
+++ modules/lambda.tf
@@ -1,3 +1,18 @@
+variable "function_name" {
+  type        = string
+  description = "The name of the Lambda function"
+}
+
+variable "handler" {
+  type        = string
+  description = "The entry point for the Lambda function, in the form of 'filename.method'"
+}
+
+variable "runtime" {
+  type        = string
+  description = "The runtime environment for the Lambda function"
+}
+
 resource "aws_iam_role" "lambda_role" {
         name = "lambda_role"
         assume_role_policy = jsonencode({
@@ -17,8 +32,8 @@ resource "aws_iam_role" "lambda_role" {
 
 resource "aws_lambda_function" "terraform_lambda_func" {
         filename                       = "${path.module}/python/hello.zip"
-        function_name                  = "Lambda_Function"
+        function_name                  = var.function_name
         role                           = aws_iam_role.lambda_role.arn
-        handler                        = "index.lambda_handler"
-        runtime                        = "python3.8"
+        handler                        = var.handler
+        runtime                        = var.runtime
 }
