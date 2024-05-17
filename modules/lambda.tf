6eeb4c73c149dd631a5831a70bd894beb28a1aa3
David Aristakesyan <45460144+DavoA@users.noreply.github.com>
2024-05-17 20:28:07 +0400

    Update lambda.tf

--- modules/lambda.tf
+++ modules/lambda.tf
@@ -15,3 +15,10 @@ resource "aws_iam_role" "lambda_role" {
         })
 }
 
+resource "aws_lambda_function" "terraform_lambda_func" {
+        filename                       = "${path.module}/python/hello.zip"
+        function_name                  = "Lambda_Function"
+        role                           = aws_iam_role.lambda_role.arn
+        handler                        = "index.lambda_handler"
+        runtime                        = "python3.8"
+}
